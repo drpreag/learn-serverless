3f75a7914893e31d87380a5bb55c737d06b39e26
Predrag Vlajkovic <predrag.vlajkovic@daon.com>
2020-08-08 13:02:26 +0200

    fix conflicts

--- root.py
+++ root.py
@@ -36,20 +36,11 @@ def hello_get(event, context):
     return response
 
 def hello_post(event, context):
-<<<<<<< HEAD
     message = "Hi, this is /hello POST endpoint!"
 
     if event["pathParameters"] is not None:
         if event["pathParameters"]["name"] is not None:
             message = "Hello man " + event["pathParameters"]["name"] + " !"
-=======
-    message = "/hello post request"
-    request = []
-    dict = json.loads(event["body"])
-
-    for key, value in dict.items():
-        request.append ([key, value])
->>>>>>> 6ae23fd96e85454197a1d121e9fc0edf8a7562dc
 
     body = {
         "message": message,
